5a4d308ff58d91fdc79f98e5dfe063f7f5056bbe
ayushmohanty42033-ctrl <ayushmohanty42033@gmail.com>
2026-07-10 09:27:29 +0530

    Seed default visitor account on database initialization

--- nalco-visitor-pass/src/main/java/com/nalco/visitorpass/config/DatabaseSeeder.java
+++ nalco-visitor-pass/src/main/java/com/nalco/visitorpass/config/DatabaseSeeder.java
@@ -4,10 +4,12 @@ import com.nalco.visitorpass.entity.Blacklist;
 import com.nalco.visitorpass.entity.Department;
 import com.nalco.visitorpass.entity.Employee;
 import com.nalco.visitorpass.entity.User;
+import com.nalco.visitorpass.entity.Visitor;
 import com.nalco.visitorpass.repository.BlacklistRepository;
 import com.nalco.visitorpass.repository.DepartmentRepository;
 import com.nalco.visitorpass.repository.EmployeeRepository;
 import com.nalco.visitorpass.repository.UserRepository;
+import com.nalco.visitorpass.repository.VisitorRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Component;
@@ -17,15 +19,18 @@ import java.util.Arrays;
 public class DatabaseSeeder implements CommandLineRunner {
 
     private final UserRepository userRepository;
+    private final VisitorRepository visitorRepository;
     private final DepartmentRepository departmentRepository;
     private final EmployeeRepository employeeRepository;
     private final BlacklistRepository blacklistRepository;
 
     public DatabaseSeeder(UserRepository userRepository,
+                          VisitorRepository visitorRepository,
                           DepartmentRepository departmentRepository,
                           EmployeeRepository employeeRepository,
                           BlacklistRepository blacklistRepository) {
         this.userRepository = userRepository;
+        this.visitorRepository = visitorRepository;
         this.departmentRepository = departmentRepository;
         this.employeeRepository = employeeRepository;
         this.blacklistRepository = blacklistRepository;
@@ -47,7 +52,30 @@ public class DatabaseSeeder implements CommandLineRunner {
             System.out.println("Seeded administrator: admin@nalcoindia.co.in");
         }
 
-        // 2. Seed Departments
+        // 2. Seed Default Visitor
+        if (userRepository.findByEmail("visitor@nalcoindia.co.in").isEmpty()) {
+            User visitorUser = new User(
+                "visitor@nalcoindia.co.in",
+                "9876543210",
+                encoder.encode("Visitor@Nalco2026"),
+                "ROLE_VISITOR"
+            );
+            userRepository.save(visitorUser);
+
+            Visitor visitor = new Visitor();
+            visitor.setUser(visitorUser);
+            visitor.setFullName("Test Visitor");
+            visitor.setGovtIdType("Aadhaar");
+            visitor.setGovtIdNumber("1234-5678-9012");
+            visitor.setCompany("Nalco Contractor");
+            visitor.setAddress("Damanjodi, Koraput, Odisha");
+            visitor.setEmergencyContact("9999999999");
+            visitor.setVehicleNumber("OD-02-AB-1234");
+            visitorRepository.save(visitor);
+            System.out.println("Seeded test visitor: visitor@nalcoindia.co.in");
+        }
+
+        // 3. Seed Departments
         if (departmentRepository.count() == 0) {
             departmentRepository.saveAll(Arrays.asList(
                 new Department("Corporate Office", "CO"),
@@ -61,7 +89,7 @@ public class DatabaseSeeder implements CommandLineRunner {
             System.out.println("Seeded organizational departments");
         }
 
-        // 3. Seed Employees
+        // 4. Seed Employees
         if (employeeRepository.count() == 0) {
             employeeRepository.saveAll(Arrays.asList(
                 new Employee("Sri A. K. Senapati", "ak.senapati@nalcoindia.co.in", "+91 94370 12345", "Smelter Plant"),
@@ -73,7 +101,7 @@ public class DatabaseSeeder implements CommandLineRunner {
             System.out.println("Seeded host employee directory");
         }
 
-        // 4. Seed Blacklisted Visitors
+        // 5. Seed Blacklisted Visitors
         if (blacklistRepository.count() == 0) {
             blacklistRepository.save(new Blacklist(
                 "Aadhaar",
